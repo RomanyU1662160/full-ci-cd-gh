4dbaa43485401275b25799aecd1ca0fb4e1a47fa
Romany-GL <romany.sefen@globallogic.com>
2024-05-01 00:26:54 +0100

    fix: flagsmith secret

--- src/components/Products/Products.test.tsx
+++ src/components/Products/Products.test.tsx
@@ -33,21 +33,55 @@ const mockProducts: Product[] = [
 const mockServerState = {
   api: 'https://edge.api.flagsmith.com/api/v1/',
   environmentID: 'test',
-  flags: { view_product_details: { id: 92031, enabled: false, value: null } },
+  flags: { view_product_details: { id: 92031, enabled: true, value: null } },
   identity: undefined,
   ts: null,
   traits: {},
   evaluationEvent: null,
 };
 
-test('renders products list', () => {
-  render(
-    <FeaturesFlagProvider serverState={mockServerState}>
-      <ProductsList products={mockProducts} />
-    </FeaturesFlagProvider>
-  );
+describe('renders products list', () => {
+  it('should render correctly the products list', () => {
+    render(
+      <FeaturesFlagProvider serverState={mockServerState}>
+        <ProductsList products={mockProducts} />
+      </FeaturesFlagProvider>
+    );
 
-  expect(screen.getByText('Products list')).toBeInTheDocument();
-  expect(screen.getByText('Product 1')).toBeInTheDocument();
-  expect(screen.getByText('Product 2')).toBeInTheDocument();
+    expect(screen.getByText('Products list')).toBeInTheDocument();
+    expect(screen.getByText('Product 1')).toBeInTheDocument();
+    expect(screen.getByText('Product 2')).toBeInTheDocument();
+    expect(screen.getAllByRole('img')).toHaveLength(2);
+    expect(screen.getAllByRole('button')).toHaveLength(2);
+  });
+
+  it('should render correctly the products list with no products', () => {
+    render(
+      <FeaturesFlagProvider serverState={mockServerState}>
+        <ProductsList products={[]} />
+      </FeaturesFlagProvider>
+    );
+
+    expect(screen.getByText('Products list')).toBeInTheDocument();
+  });
+
+  it("should not render the product's details button if the feature flag is disabled", () => {
+    const flagDisabled = {
+      view_product_details: { id: 92031, enabled: false, value: null },
+    };
+    render(
+      <FeaturesFlagProvider
+        serverState={{
+          ...mockServerState,
+          flags: { ...mockServerState.flags, ...flagDisabled },
+        }}
+      >
+        <ProductsList products={mockProducts} />
+      </FeaturesFlagProvider>
+    );
+
+    expect(
+      screen.queryByRole('button', { name: 'More info' })
+    ).not.toBeInTheDocument();
+  });
 });
